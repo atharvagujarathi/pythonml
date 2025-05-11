59606a3938afcf22fd2e3d40284befc3df62fec7
atharvagujarathi <75681555+atharvagujarathi@users.noreply.github.com>
2025-05-12 00:44:27 +0530

    oops will start

---
+++ python/3.import-export-modules/myimport.py
@@ -0,0 +1,4 @@
+from mypython import add, _private_helper, PI
+
+print(add(2, 3))
+
---
+++ python/3.import-export-modules/mypython.py
@@ -0,0 +1,7 @@
+def add(a, b):
+    return a + b
+
+def _private_helper():
+    pass  # Not intended for export (by convention)
+
+PI = 3.14
